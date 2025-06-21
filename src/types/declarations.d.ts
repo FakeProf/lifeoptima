24603462e3a195b72747724aefd85f6c7186e648
Julian Steiner <mail@juliansteiner.de>
2025-06-21 23:49:18 +0200

    CLEAN: Nur essentielle Web-App Dateien - index.html + README.md

--- src/types/declarations.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'react-native-vector-icons/MaterialIcons';
-declare module 'react-native-linear-gradient';
-declare module 'react-native-push-notification';
-declare module 'react-native-chart-kit';
-declare module 'react-native-svg'; 
